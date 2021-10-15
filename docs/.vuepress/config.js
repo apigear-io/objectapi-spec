78bbe593ce957d7464f3e679693dc631ff500667
Juergen Ryannel <juergen@ryannel.org>
2021-10-15 15:29:37 +0200

    update config.js to set correct base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,34 +1,27 @@
 module.exports = {
-    title: 'ObjectAPI',
-    description: 'Create the future of API driven architectures',
-    themeConfig: {
-        sidebarDepth: 3,
-        nav: [
-            { 
-                text: 'Guide',
-                items: [
-                    { text: 'Introduction', link: '/guide/intro' },
-                    { text: 'Tutorial', link: '/guide/tutorial' },
-                    { text: 'Specification', link: '/guide/specification' },
-                ]
-            },
-            { 
-                text: 'Tools',
-                items: [
-                    { text: 'Generator', link: '/tools/generator' },
-                ]
-            },
-            { text: 'Community', link: '/community/' }
-        ],
-        sidebar: {
-            '/guide/': [
-                'intro',
-                'tutorial',
-                'specification'
-            ],
-            '/tools/': [
-                'generator',
-            ]
-        }
+  base: '/objectapi-site',
+  title: 'ObjectAPI',
+  description: 'Create the future of API driven architectures',
+  themeConfig: {
+    sidebarDepth: 3,
+    nav: [
+      {
+        text: 'Guide',
+        items: [
+          { text: 'Introduction', link: '/guide/intro' },
+          { text: 'Tutorial', link: '/guide/tutorial' },
+          { text: 'Specification', link: '/guide/specification' }
+        ]
+      },
+      {
+        text: 'Tools',
+        items: [{ text: 'Generator', link: '/tools/generator' }]
+      },
+      { text: 'Community', link: '/community/' }
+    ],
+    sidebar: {
+      '/guide/': ['intro', 'tutorial', 'specification'],
+      '/tools/': ['generator']
     }
   }
+}
